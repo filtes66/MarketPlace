3e5edfd814e3b45a9dcd8db253104beeb05f0339
philippe teisseyre <philippeteisseyre@philippes-MBP.lan>
2024-04-24 17:15:46 +0200

    coding diplay of cart page

--- src/components/Panier/Panier.js
+++ src/components/Panier/Panier.js
@@ -9,7 +9,6 @@ import "./Panier.css";
 function Panier() {
   const dispatch = useDispatch();
   const { items, totalPrice, numberItems } = useSelector((state) => state.cart);
-
   const onTrashClick = (item) => dispatch(removeFromCart(item));
 
 
@@ -17,10 +16,15 @@ function Panier() {
   const licence = "libre de droits";
 
 
-  const Item = ({ url, nom, id, prix, arrond }) => (
+  const Item = ({ url, nom, id, prix, arrond, cartScaledHeight, cartScaledWidth }) => (
     <div className="panier-item__flex">
       <div className="panier-item__photo">
-        <PhotoRender url={url} arrond={arrond} currentHeight="110px" />
+        { /*    <PhotoRender url={url} arrond={arrond} currentHeight="110px" />*/}
+        <img className="photo__image"
+          src={`/image/${arrond}/${url}.jpg`}
+          height={cartScaledHeight}
+          width={cartScaledWidth}
+          alt={`Photo from ${arrond}`} />
       </div>
       <div className="panier-item__description">
         <div className="panier-item__nom-flex">
@@ -38,7 +42,7 @@ function Panier() {
       </div>
       <div className="panier-item__prix-flex">
         <div className="panier-item__trash" onClick={() => onTrashClick(id)}>
-          <FaTrash />
+          X
         </div>
         <div className="panier-item__price">{prix} €</div>
       </div>
@@ -46,8 +50,8 @@ function Panier() {
   );
 
   const ListeItems = ({ items }) => {
-    const listeItems = items.map(({ url, nom, id, prix, arrond }) => (
-      <Item key={id} url={url} nom={nom} id={id} prix={prix} arrond={arrond} />
+    const listeItems = items.map(({ url, nom, id, prix, arrond, cartScaledHeight, cartScaledWidth }) => (
+      <Item key={id} url={url} nom={nom} id={id} prix={prix} arrond={arrond} cartScaledHeight={{ cartScaledHeight }} cartScaledWidth={cartScaledWidth} />
     ));
     return <>{listeItems}</>;
   };
--- src/store/reducers/photos/returnPhotosArrays.js
+++ src/store/reducers/photos/returnPhotosArrays.js
@@ -1,47 +1,72 @@
-//const GRID_WIDTH = 1200;
+// Constants
 const HEIGHT_RATIO = 300;
 const WIDTH_LARGE = 552;
 const WIDTH_SMALL = 370;
+const CART_WIDTH_LARGE = 170;
+const CART_WIDTH_SMALL = 134;
 const WIDTH_RATIO_UPPER = 1.2;
 const WIDTH_RATIO_LOWER = 0.8;
+const DISTRICT_COUNT = 20;
 
+// Helper function to calculate scaled sizes
+/*function calculateScaledSizes(item, largeWidth, smallWidth) {
+    let scaledWidth;
+    let scaledHeight;
+    let cartScaledWidth;
+    let cartScaledHeight;
+    if (item.width > item.height) {
+        scaledHeight = (HEIGHT_RATIO * largeWidth) / item.width;
+        scaledWidth = largeWidth;
+        cartScaledWidth = smallWidth;
+        cartScaledHeight = (HEIGHT_RATIO * smallWidth) / item.width;
+    } else {
+        scaledHeight = (HEIGHT_RATIO * smallWidth) / item.width;
+        scaledWidth = smallWidth;
+        cartScaledHeight = HEIGHT_RATIO;
+        cartScaledWidth = (HEIGHT_RATIO * smallWidth) / item.height;
+    }
+    return { scaledWidth, scaledHeight, cartScaledWidth, cartScaledHeight };
+}*/
+
+// Main function
 function returnPhotosArrays(photoItems, windowSize) {
-    let photoItems2 = JSON.parse(JSON.stringify(photoItems));
-    console.log('photoItems, windowSize', photoItems, windowSize)
+    let photoItemsCopy = JSON.parse(JSON.stringify(photoItems));
     let GRID_WIDTH = windowSize;
     const districtsPhotos = [];
     const neighborhoodPhotos = [];
     const adminPhotos = [];
     const ParisPhotos = [];
-    let cond = true;
-    for (let i = 1; i <= 20; i++) {
+    let isFirstIteration = true;
+
+    for (let i = 1; i <= DISTRICT_COUNT; i++) {
         let districtPhotos = [];
-        photoItems2.forEach((item) => {
+        photoItemsCopy.forEach((item) => {
             if (item.arrond === i) {
                 districtPhotos.push(item);
             }
-            if (item.quartier && cond) {
+            if (item.quartier && isFirstIteration) {
                 neighborhoodPhotos.push(item);
             }
-            if (item.bat && cond) {
+            if (item.bat && isFirstIteration) {
                 adminPhotos.push(item);
             }
-            cond && ParisPhotos.push(item);
+            isFirstIteration && ParisPhotos.push(item);
         });
-        cond = false;
+        isFirstIteration = false;
         let districtPhotosCopy = JSON.parse(JSON.stringify(districtPhotos));
         districtsPhotos.push(createPhotoGrid(districtPhotosCopy, GRID_WIDTH));
-
     }
+
     const photosObject = {
         ParisPhotos: createPhotoGrid(JSON.parse(JSON.stringify(ParisPhotos)), GRID_WIDTH),
         districtsPhotos: districtsPhotos,
         adminPhotos: createPhotoGrid(adminPhotos, GRID_WIDTH),
         neighborhoodPhotos: createPhotoGrid(neighborhoodPhotos, GRID_WIDTH),
-
     };
-    return photosObject
+
+    return photosObject;
 }
+
 
 function createPhotoGrid(photoItems, GRID_WIDTH) {
     let currentRow = [];
@@ -50,12 +75,28 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
     let currentHeight = 0;
     let scaledHeight = 0;
     let scaledWidth = 0;
+    let cartScaledHeight = 0;
+    let cartScaledWidth = 0;
     let maxColumns = 0;
     let currentColumns = 0;
     let previousMaxColumns = 0;
     let currentItem = 0;
     let currentRowNumber = 0;
     let totalHeight = 0;
+
+    function calculateScaledSizes(item) {
+        if (item.width > item.height) {
+            scaledHeight = (HEIGHT_RATIO * WIDTH_LARGE) / item.width;
+            scaledWidth = WIDTH_LARGE;
+            cartScaledWidth = CART_WIDTH_LARGE;
+            cartScaledHeight = (HEIGHT_RATIO * CART_WIDTH_LARGE) / item.width;
+        } else {
+            scaledHeight = (HEIGHT_RATIO * WIDTH_SMALL) / item.width;
+            scaledWidth = WIDTH_SMALL;
+            cartScaledHeight = (HEIGHT_RATIO * CART_WIDTH_SMALL) / item.width;
+            cartScaledWidth = (HEIGHT_RATIO * CART_WIDTH_SMALL) / item.height;
+        }
+    }
 
     const handleLargeItem = (photoItem) => {
         currentRow.pop();
@@ -66,6 +107,7 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
         currentRowNumber += 1;
         scaledHeight = (HEIGHT_RATIO * WIDTH_LARGE) / photoItem.width;
         scaledWidth = WIDTH_LARGE;
+        cartScaledHeight = (HEIGHT_RATIO * CART_WIDTH_LARGE) / photoItem.width;
         console.log('currentHeight, currentWidth', currentHeight, GRID_WIDTH);
         allRows.push([
             {
@@ -76,6 +118,8 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
                 gridWidth: GRID_WIDTH,
                 scaledHeight: scaledHeight,
                 scaledWidth: scaledWidth,
+                cartScaledHeight: cartScaledHeight,
+                cartScaledWidth: CART_WIDTH_LARGE,
                 currentWidth: GRID_WIDTH
             },
         ]);
@@ -103,13 +147,12 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
         currentRow = currentRow.map((item) => {
             let aspectRatio = item.width / item.height;
             let currentWidth = currentHeight * aspectRatio;
-            if (item.width > item.height) {
-                scaledHeight = (HEIGHT_RATIO * WIDTH_LARGE) / item.width;
-                scaledWidth = WIDTH_LARGE;
-            } else {
-                scaledHeight = (HEIGHT_RATIO * WIDTH_SMALL) / item.width;
-                scaledWidth = WIDTH_SMALL;
-            }
+            // Calculate scaled sizes for the presentation item page
+            calculateScaledSizes(item);
+
+            // Calculate smaller scaled sizes for the shopping cart page
+            calculateScaledSizes(item);
+
             return {
                 ...item,
                 currentHeight: currentHeight,
@@ -117,6 +160,8 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
                 gridWidth: GRID_WIDTH,
                 scaledHeight: scaledHeight,
                 scaledWidth: scaledWidth,
+                cartScaledHeight: cartScaledHeight,
+                cartScaledWidth: cartScaledWidth,
                 currentWidth: currentWidth
             };
         });
@@ -133,13 +178,12 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
         currentRow = currentRow.map((item) => {
             let aspectRatio = item.width / item.height;
             let currentWidth = currentHeight * aspectRatio;
-            if (item.width > item.height) {
-                scaledHeight = (HEIGHT_RATIO * WIDTH_LARGE) / item.width;
-                scaledWidth = WIDTH_LARGE;
-            } else {
-                scaledHeight = (HEIGHT_RATIO * WIDTH_SMALL) / item.width;
-                scaledWidth = WIDTH_SMALL;
-            }
+            // Calculate scaled sizes for the presentation item page
+            calculateScaledSizes(item);
+
+            // Calculate smaller scaled sizes for the shopping cart page
+            calculateScaledSizes(item);
+
             return {
                 ...item,
                 currentHeight: currentHeight,
@@ -147,6 +191,8 @@ function createPhotoGrid(photoItems, GRID_WIDTH) {
                 gridWidth: GRID_WIDTH,
                 scaledHeight: scaledHeight,
                 scaledWidth: scaledWidth,
+                cartScaledHeight: cartScaledHeight,
+                cartScaledWidth: cartScaledWidth,
                 currentWidth: currentWidth
             };
         });
