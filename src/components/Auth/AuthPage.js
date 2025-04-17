87228a44ad78e80295dc26a3967a24c52e0e8dc8
philippe teisseyre <philippeteisseyre@Host-002.lan>
2025-04-17 17:48:36 +0200

    useless dependancy deleted

--- src/components/Auth/AuthPage.js
+++ src/components/Auth/AuthPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-//import { useLocation } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import './AuthPage.css';
